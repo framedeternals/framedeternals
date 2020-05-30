b4e142983b894e7e247f975bb2c250fa03ad92fb
framedeternals <66163991+framedeternals@users.noreply.github.com>
2020-05-30 16:11:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("feed").addEventListener("click", displayDate);
+
+function displayDate() {
+    alert('eohg');
+    document.getElementById('thanks').innerHTML = 'rrrrgnrt'
+}
+
+
